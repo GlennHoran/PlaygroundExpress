76cd2da72cf448e96d0d01493c4e4a36daad8aa5
Glenn Horan <glennhoran88@gmail.com>
2018-02-18 16:17:14 +0000

    added accelerometer visualisation feature

--- PlaygroundExpress.ino
+++ PlaygroundExpress.ino
@@ -3,13 +3,27 @@
 void setup() {
   // Initialize the circuit playground
   CircuitPlayground.begin();
+ // Serial.begin(9600);
 }
  
 void loop() {
-  // If the left button is pressed....
-  if (CircuitPlayground.leftButton()) {
-      CircuitPlayground.redLED(HIGH);  // LED on
-  } else {
-      CircuitPlayground.redLED(LOW);   // LED off
+  for (int i=0; i<9; i++){
+    CircuitPlayground.setPixelColor(i,convertAccToInt(CircuitPlayground.motionX()), convertAccToInt(CircuitPlayground.motionY()),convertAccToInt(CircuitPlayground.motionZ()));
   }
 }
+
+int convertAccToInt(float x){
+  int neoPixelValue;
+  int i;
+  //converts accelerometer value into a positive number then multiplies by 100 to keep resolution
+  float accelerometerValueAsInt = ((x+10) *100);
+  //converts float into int
+  i = int (accelerometerValueAsInt);
+  //converts into value 0-255 for neoplixel input
+  neoPixelValue = i/9;
+return i; 
+ 
+
+}
+
+
